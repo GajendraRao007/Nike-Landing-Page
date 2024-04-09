de5960f0bc2e55894fc6959eee4c09b1302f7272
GajendraRao007 <raogajendra27@gmail.com>
2024-04-09 15:13:01 +0530

    added header component

---
+++ src/component/header.jsx
@@ -0,0 +1,22 @@
+function Header(params) {
+    return(
+        <div className="header">
+            <div className="logo">
+                <h1>LOGO</h1>
+            </div>
+            <div className="Nav-menus">
+               <ul>
+                <li>MENU</li>
+                <li>LOCATION</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+               </ul>
+            </div>
+            <div className="login-btn">
+                <button>Login</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
