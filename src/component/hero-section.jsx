e8e8dad9610787243690330b9b9a4a7834299211
GajendraRao007 <raogajendra27@gmail.com>
2024-04-09 16:26:36 +0530

    styled hero-section component

--- src/component/hero-section.jsx
+++ src/component/hero-section.jsx
@@ -6,16 +6,16 @@ function HeroSection(){
     return(
         <div className="hero-section">
             <div>
-                <h1>YOUR FEET DESERVE THE BEST</h1>
-                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <h1 className='hero-title'>YOUR FEET DESERVE THE BEST</h1>
+                <p className='text'>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
                 <div className="two-btns">
-                        <button>Shop Now</button>
-                        <button>Category</button>
+                        <button className='shop-btn'>Shop Now</button>
+                        <button className='category-btn'>Category</button>
                 </div>
-                <p>Also Available On</p>
-                <div className="">
-                    <img src={FlipkartImg} alt="" srcset="" />
-                    <img src={AmazonImg} alt="" srcset="" />
+                <p className='img-title'>Also Available On</p>
+                <div className="two-imgs">
+                    <img className='small-imgs' src={FlipkartImg} alt="" srcset="" />
+                    <img className='small-imgs' src={AmazonImg} alt="" srcset="" />
                 </div>
             </div>
 
